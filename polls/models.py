fc5f592871ba446a4d3d6dea9deab7fd3fed4e86
yeming <492851378@qq.com>
2018-09-22 13:01:12 +0800

    Myuser。。。

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.base_user import AbstractBaseUser
+
 
 # Create your models here.
 
@@ -30,12 +31,41 @@ class Article(models.Model):
         return self.title
 
 
+# ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓直接继承AbstractUser能够有效运行↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 # 创建一个自定义的用户模型
 class MyUser(AbstractUser):
+
+    score = models.IntegerField('积分', default=0)
+
+    # Give your model metadata(元数据) by using an inner class Meta
+    # Model metadata is “anything that’s not a field”,
+    # such as ordering options, database table name,
+    # or human-readable singular(单数) and plural(复数) names.
+    # None are required, and adding class Meta to a model is completely optional.
+    # 子类：元数据，设置一些属性。
+    # 数据库中显示的名称
+    class Meta(AbstractUser.Meta):
+        db_table = 'Myuser'
+
+    # Shell中实例显示的名称
+    def __str__(self):
+        return self.username
+
+# ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑直接继承AbstractUser能够有效运行↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+
+# ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓继承AbstractBaseUser↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+# 这个类少了username, email，需要自己定制，直接拿源码会报错，username = AbstractUser.username,也会报错
+
+# class Myuser(AbstractBaseUser):
+    # objects = UserManager()
+
     # 要继承AbstractBaseUser, 一定要有两个属性：identifier、 USERNAME_FIELD
     # you model must be have a single unique field that can be used for identification purposes.
     # identifier = models.CharField(max_length=40, unique=True)
     # USERNAME_FIELD = 'identifier'
+    #
+    # 源码的username
     # username_validator = UnicodeUsernameValidator()
     #
     # username = models.CharField(
@@ -49,25 +79,39 @@ class MyUser(AbstractUser):
     #     },
     # )
 
-    first_name = None
-    last_name = None
-    is_active = None
-    is_staff = None
-    is_superuser = None
+    # email = models.EmailField(_('email address'), blank=True)
 
-    score = models.IntegerField('积分', default=0)
+    # EMAIL_FIELD = 'email'
+    #
+    # class Meta(AbstractUser.Meta):
+    #     db_table = 'Myuser'
+    #
+    # # Shell中实例显示的名称
+    # def __str__(self):
+    #     return self.username
 
-    # Give your model metadata(元数据) by using an inner class Meta
-    # Model metadata is “anything that’s not a field”,
-    # such as ordering options, database table name,
-    # or human-readable singular(单数) and plural(复数) names.
-    # None are required, and adding class Meta to a model is completely optional.
-    # 子类：元数据，设置一些属性。
-    # 数据库中显示的名称
-    class Meta:
-        db_table = 'Myuser'
+    # def clean(self):
+    #     super().clean()
+    #     self.email = self.__class__.objects.normalize_email(self.email)
 
-    # Shell中实例显示的名称
-    def __str__(self):
-        return self.username
+# ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑继承AbstractBaseUser↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
+
+# ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓继承AbstractUser↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+# 这个用户模型有多余的Field，重写直接赋值None，报错...
+
+# class Myuser(AbstractUser):
+    # first_name = None
+    # last_name = None
+    # is_active = None
+    # is_staff = None
+    # is_superuser = None
+
+    # class Meta(AbstractUser.Meta):
+    #     db_table = 'Myuser'
+    #
+    # # Shell中实例显示的名称
+    # def __str__(self):
+    #     return self.username
+
+# ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑继承AbstractUser↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
